3ddacfcef3741f899af6d25c7f90a51ac691af16
Bas <85746482+Basgamer999@users.noreply.github.com>
2024-01-11 20:14:53 +0100

    Fixed the redstone client

--- redstone.lua
+++ redstone.lua
@@ -1,13 +1,12 @@
 local redstone = component.proxy(component.list("redstone")())
 local modem = component.proxy(component.list("modem")())
-local pin = ""
-modem.open(900)
+modem.open(port)
 
 while true do
     local type, _, from, port, _, message, status = computer.pullSignal()
     if type == "modem_message" then
         if (message == "ss"..pin) then 
-            modem.send(from, tonumber(port), "redstone", "switchone")
+            modem.send(from, tonumber(port), "redstone", name)
         elseif message == "setRedstone" then
             redstone.setOutput(1, tonumber(status))
             modem.send(from, tonumber(port), "result", "sucess")
